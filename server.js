42d87f99e24a9512300330dc5a9c832288301b8a
omprakash1907 <omprakashkjat19@gmail.com>
2024-03-17 21:34:59 +0530

    final

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ const storage = multer.diskStorage({
         return cb(null, './upload')
     },
     filename :function(req,file,cb){
-        return cb(null, file.originalname)
-        console.log(file)
+        return cb(null, Date.now() + file.originalname)
     }
 })
 
@@ -34,7 +33,6 @@ app.get('/upload', function(req,res){
 })
 
 app.post('/upload', function (req, res) {
-    console.log(req.body)
     upload(req, res, async function (err) {
         if (err) {
             console.log(err);
@@ -51,13 +49,13 @@ app.post('/upload', function (req, res) {
             year: req.body.year,
             genre: req.body.genre,
             rating: req.body.rating,
-            file: req.file.originalname
+            file: req.file.filename
         };
 
         try {
             const profile = await movieModel(details);
             const result = await profile.save();
-            res.redirect('/');
+            res.redirect('/list');
         } catch (error) {
             console.error(error);
             res.status(500).send('Error saving movie details.');
@@ -68,6 +66,70 @@ app.get('/list', async function(req,res){
     const movies = await movieModel.find({})
     res.render('./Pages/watchlist', {movies : movies})
 })
+
+//-----------DeleteData
+
+app.get('/deleteData/:id', async function(req, res){
+    var id = req.params.id
+    const image = await movieModel.findOne({_id : id})
+    const result = await movieModel.findByIdAndDelete({_id : id})
+    if(result){
+        fs.unlink(`upload/${image.file}`, (err)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log('successs..')
+        })
+        res.redirect('/list')
+    }
+})
+
+
+//-------------------editData
+
+app.get('/editData/:id', async function(req, res){
+    var id = req.params.id
+    const movies = await movieModel.findOne({_id : id})
+    res.render('./Pages/edit', {movies : movies})
+})
+
+app.post('/editData/:id', async function(req, res){
+    var id = req.params.id
+    upload(req, res, async () => {
+        try {
+            var oldMovie = await movieModel.findOne({ _id: id });
+
+            const details = {
+                name: req.body.name,
+                description: req.body.description,
+                year: req.body.year,
+                genre: req.body.genre,
+                rating: req.body.rating,
+            };
+
+            if (req.file) {
+                if (oldMovie.file) {
+                    fs.unlink(`upload/${oldMovie.file}`, (err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Old image deleted successfully");
+                        }
+                    });
+                }
+                details.file = req.file.filename;
+            } else {
+                details.file = oldMovie.file;
+            }
+            await movieModel.updateOne({ _id: id }, details);
+            res.redirect('/list');
+        } catch (error) {
+            console.error(error);
+        }
+    });
+})
+
+
 
 app.listen((3000), () =>{
     console.log('Server Started : 3000')
